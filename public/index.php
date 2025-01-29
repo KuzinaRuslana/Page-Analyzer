a7ff8336df0f94075e881b4f556ba012880c5f5c
Ruslana <kuzina.ruslana@yandex.ru>
2025-01-29 09:39:18 +0300

    Test commit # 2

--- public/index.php
+++ public/index.php
@@ -8,12 +8,11 @@ use Slim\Factory\AppFactory;
 use Slim\Flash\Messages;
 use Slim\Views\PhpRenderer;
 
-die('asd');
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->safeload();
-$dotenv->required(['DATABASE_URL'])->notEmpty();
+// $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+// $dotenv->safeload();
+// $dotenv->required(['DATABASE_URL'])->notEmpty();
 
-session_start();
+// session_start();
 
 $container = new Container();
 
@@ -21,30 +20,30 @@ $container->set('renderer', function () {
     return new PhpRenderer(__DIR__ . '/../templates');
 });
 
-$container->set('flash', function () {
-    return new Messages();
-});
+// $container->set('flash', function () {
+//     return new Messages();
+// });
 
-$container->set(\PDO::class, function () {
-    $databaseUrl = $_ENV['DATABASE_URL'] ?? null;
+// $container->set(\PDO::class, function () {
+//     $databaseUrl = $_ENV['DATABASE_URL'] ?? null;
 
-    $parsedUrl = parse_url($databaseUrl);
-    $host = $parsedUrl['host'];
-    $port = $parsedUrl['port'] ?? '5432';
-    $dbname = ltrim($parsedUrl['path'], '/');
-    $username = $parsedUrl['user'];
-    $password = $parsedUrl['pass'];
+//     $parsedUrl = parse_url($databaseUrl);
+//     $host = $parsedUrl['host'];
+//     $port = $parsedUrl['port'] ?? '5432';
+//     $dbname = ltrim($parsedUrl['path'], '/');
+//     $username = $parsedUrl['user'];
+//     $password = $parsedUrl['pass'];
 
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+//     $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
 
-    $conn = new \PDO($dsn, $username, $password);
-    $conn->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+//     $conn = new \PDO($dsn, $username, $password);
+//     $conn->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
 
-    return $conn;
-});
+//     return $conn;
+// });
 
 $app = AppFactory::createFromContainer($container);
-$app->addErrorMiddleware(true, true, true);
+// $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
     return $this->get('renderer')->render($response, 'index.phtml');
